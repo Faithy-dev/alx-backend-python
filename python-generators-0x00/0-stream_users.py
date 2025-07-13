50457e1faf973dcd04d0dfa771be59942a200983
Faithy.dev <fathiaolusegun@gmail.com>
2025-07-13 04:43:32 +0100

    Create 0-stream_users.py

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def stream_users():
+    """Generator function that yields one user row at a time from user_data table"""
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ALX_prodev"
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
